cf5c11d45cd0da8725dc5a8277950a9a5acf74cd
Alexander Kofi Stone <90461224+AlexanderKofiStone@users.noreply.github.com>
2022-01-20 20:55:08 +0000

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    css: {
+      loaderOptions: {
+        scss: {
+          additionalData: `
+            @import "@/scss/_variables.scss";
+            @import "@/scss/_mixins.scss";
+          `
+        }
+      }
+    }
+  };
